483e1cc721b573419dc40d8db7b20ff20890fc8a
aistenjack <aistenjack@gmail.com>
2021-03-22 09:50:30 -0400

    Utimos

--- delivery_backend/settings.py
+++ delivery_backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from decouple import config
-
+from dj_database_url import parse as dburl
 from pathlib import Path
 
 import os
