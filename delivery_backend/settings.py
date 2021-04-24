174aa0d6c4d4f918d2b969bae23454b2fa197447
Vilkson Gonçalves Freitas <aistenjack@gmail.com>
2021-04-24 14:21:16 -0400

    testando erro 500 05

--- delivery_backend/settings.py
+++ delivery_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
